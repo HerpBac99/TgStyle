80acd9f863e729d2ba958f54f6561714b5857eb6
Herp_Bac9 <gr.podolny@yandex.ru>
2025-09-14 11:21:38 +0300

    14.09.2025

--- fastvlm-server/test_server_api.py
+++ fastvlm-server/test_server_api.py
@@ -13,7 +13,7 @@ import requests
 from pathlib import Path
 
 # Configuration
-TEST_IMAGE_PATH = "6.jpg"
+TEST_IMAGE_PATH = "12.jpg"
 SERVER_URL = "http://127.0.0.1:3001"
 
 def load_image_as_base64(image_path):
